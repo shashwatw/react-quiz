08c6f6a97b26d793dc89e1a0a74cdf99cd53f7f9
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-09-01 10:55:43 +0530

    Splitting options component

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ const initialState = {
 
   //"loading", "error", "ready", "active", "finished"
   status: "loading",
+  index: 0,
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -37,7 +38,10 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numQuestions = questions.length;
 
@@ -59,7 +63,7 @@ export default function App() {
         <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
       )}
 
-      {status === "active" && <Question />}
+      {status === "active" && <Question question={questions[index]} />}
     </div>
   );
 }
